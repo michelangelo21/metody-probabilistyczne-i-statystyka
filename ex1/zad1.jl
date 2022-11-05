c40916b74bc59efb9f9d2e4268a3db1e4ad35e71
Michał Łukomski <michal.lukomski21@gmail.com>
2022-11-05 22:01:55 +0100

    move zad1

--- ex1/zad1.jl
+++ ex1/zad1.jl
@@ -7,6 +7,9 @@ Random.seed!(42)
 plotlyjs()
 
 function approximate_integral(foo::Function, a::Number, b::Number, n::Int, M::Number)
+    """
+    Approximates the integral of a function foo on the interval [a, b] using the Monte Carlo method.
+    """
     x = rand(Uniform(a, b), n)
     y = rand(Uniform(0, M), n)
 
@@ -20,9 +23,9 @@ approximate_integral(x -> ∛x, 0, 8, 50, 10)
 
 maximum(∛x for x in 0:0.0001:8)
 
-foo(x) = ∛x
-foo(x) = sin(x)
-foo(x) = 4 * x * (1 - x)^3
+foo(x) = ∛x # =12
+foo(x) = sin(x) # =2
+foo(x) = 4 * x * (1 - x)^3 # =0.2
 
 
 a = 0
@@ -30,22 +33,25 @@ b = 8
 # M = 10 * maximum(foo(x) for x in a:0.001:b)
 M = 2
 
+true_integral = 12
+
 k = 50
 
 ns = 50:50:5000
-integrals(n) = [approximate_integral(foo, a, b, n, M) for i in 1:k]
+integrals(n) = [approximate_integral(foo, a, b, n, M) for _ in 1:k]
 results = [integrals(n) for n in ns]
 
 scatter(collect(Iterators.flatten(ones(k) * ns')),
     collect(Iterators.flatten(results)),
-    marker=(2, :blue, stroke(0)),
-    label="single repetitions"
+    marker=(1, :blue, stroke(0)),
+    label="single probe"
 )
-scatter!(ns, mean.(results), marker=(:red, stroke(0)), label="mean")
-hline!([12], color=:green, label="true value")
+scatter!(ns, mean.(results), marker=(2, :red, stroke(0)), label="mean")
+hline!([12], linewidth=2, color=:green, label="true value")
 xlabel!("n")
 ylabel!("integral")
 title!("∫₀⁸ ∛x dx")
+savefig
 
 # symbolic integration
 using Symbolics
